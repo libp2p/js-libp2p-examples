7cc175ad9990000f573dd1fe5ce7aa3b661a4a38
achingbrain <alex@achingbrain.net>
2023-12-04 12:10:25 +0000

    chore: update example

--- examples/js-libp2p-example-delegated-routing/src/libp2p.js
+++ examples/js-libp2p-example-delegated-routing/src/libp2p.js
@@ -1,17 +1,12 @@
 import { createDelegatedRoutingV1HttpApiClient } from '@helia/delegated-routing-v1-http-api-client'
-import { webSockets } from '@libp2p/websockets'
 import { createLibp2p } from 'libp2p'
 
 /**
- * Creates a basic libp2p node - no listen addresses (so dial-only), no muxers
- * or encrypters, and no content/peer routing modules apart from the delegated
- * routing v1 http api client
+ * Creates a basic libp2p node - it will only perform peer/content routing via
+ * the configured delegated routing v1 http api client
  */
 export async function configureLibp2p () {
   return await createLibp2p({
-    transports: [
-      webSockets()
-    ],
     services: {
       delegatedRouting: () => createDelegatedRoutingV1HttpApiClient('http://127.0.0.1:9832')
     }
