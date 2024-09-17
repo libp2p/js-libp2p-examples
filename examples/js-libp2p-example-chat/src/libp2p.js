2f749e0bc1b011b4105c3d6f25f55532f341bd7d
achingbrain <alex@achingbrain.net>
2024-09-17 18:02:36 +0200

    chore: linting

--- examples/js-libp2p-example-chat/src/libp2p.js
+++ examples/js-libp2p-example-chat/src/libp2p.js
@@ -1,10 +1,10 @@
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
+import { mdns } from '@libp2p/mdns'
 import { tcp } from '@libp2p/tcp'
 import { webSockets } from '@libp2p/websockets'
 import defaultsDeep from '@nodeutils/defaults-deep'
 import { createLibp2p as create } from 'libp2p'
-import { mdns } from '@libp2p/mdns'
 
 export async function createLibp2p (_options) {
   const defaults = {
