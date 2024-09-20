ad2f94c472e1cdcd66c07ac78a2e1739ac7505b4
achingbrain <alex@achingbrain.net>
2024-09-20 17:52:41 +0200

    chore: tests

--- examples/js-libp2p-example-pubsub/test/test-2.js
+++ examples/js-libp2p-example-pubsub/test/test-2.js
@@ -7,7 +7,16 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 export async function test () {
   process.stdout.write('2.js\n')
 
-  await waitForOutput('node3 received: orange', 'node', [path.join(__dirname, '../2.js')], {
-    cwd: __dirname
-  })
+  try {
+    await waitForOutput('node3 received: car', 'node', [path.join(__dirname, '../2.js')], {
+      cwd: __dirname,
+      timeout: 2000
+    })
+
+    throw new Error('Matched content when should not have')
+  } catch (err) {
+    if (err.message !== 'Timed out' && !err.message.includes('Did not see')) {
+      throw err
+    }
+  }
 }
